c596b694f6f6edde1df2ea4b1984362f0b5181e8
Debora Gomez <Debora.Gomez@mytoys.de>
2018-06-17 20:04:49 +0200

    Allow CORS

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ var express = require('express'),
   port = process.env.PORT || 3000;
   bodyParser = require('body-parser');
 
+  //CORS middleware
+  var allowCrossDomain = function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
+      next();
+  }
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(allowCrossDomain);
+
 // When a route is used, then return 404
 // app.use(function(req, res) {
 //   res.status(404).send({url: req.originalUrl + ' not found'})
