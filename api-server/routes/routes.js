f8056b2c41afed472da1b892bb86221ee6e8c835
Debora Gomez <Debora.Gomez@mytoys.de>
2018-07-05 20:20:19 +0200

    Get task - solution

--- api-server/routes/routes.js
+++ api-server/routes/routes.js
@@ -4,11 +4,7 @@ module.exports = function(app) {
 
   app.route('/tasks')
     .get(todoList.list_all_tasks);
-  app.route('/task')
-    .post(todoList.create_a_task);
 
   app.route('/task/:taskId')
     .get(todoList.read_a_task)
-    .put(todoList.update_a_task)
-    .delete(todoList.delete_a_task);
 };
