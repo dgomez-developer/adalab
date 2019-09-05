fa623ae581624ccc8c036a3d5de7d0f35d8bd287
Debora Gomez <Debora.Gomez@mytoys.de>
2019-09-05 09:56:55 +0200

    Allow global cors

--- heroines-api/src/main/kotlin/com/dgomez/developer/heroines/configuration/DefaultConfiguration.kt
+++ heroines-api/src/main/kotlin/com/dgomez/developer/heroines/configuration/DefaultConfiguration.kt
@@ -4,14 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+
 
 @Configuration
-class DefaultConfiguration {
+@EnableWebMvc
+class DefaultConfiguration: WebMvcConfigurer {
 
     @Bean
     fun includeTransientObjectMapper(): ObjectMapper {
         val mapper = ObjectMapper()
         mapper.registerModule(KotlinModule())
         return mapper
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
     }
 }
